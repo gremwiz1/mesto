c1c1fce58529d37222e344830a1bff651f05042d
Gremwiz <gremwiz@yandex.ru>
2021-02-23 01:41:56 +0300

    update code

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,16 @@
 let formElement = document.querySelector('.poppup-container');
 let poppup = document.querySelector('.poppup');
-let nameInput = document.querySelector('.poppup-container__name');
-let jobInput = document.querySelector('.poppup-containter__prof');
+let formelements = document.querySelectorAll('.poppup-container__name');
+
+let nameInput = formelements[0];
+let jobInput = formelements[1];
 let poppup_close = document.querySelector('.poppup-container__button-close');
 let poppup_open = document.querySelector('.profile-button');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     let profile__title = document.querySelector('.profile__title');
-    let profile__profession = document.querySelector('.profile__prof');
+    let profile__profession = document.querySelector('.profile__profession');
     profile__title.textContent = nameInput.value;
     profile__profession.textContent = jobInput.value;
     
