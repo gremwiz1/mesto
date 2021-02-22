b8a0a9ca2c2fd66d93f9b081196f5a8a2a389130
Gremwiz <gremwiz@yandex.ru>
2021-02-23 00:13:33 +0300

    added poppup

---
+++ pages/script.js
@@ -0,0 +1,33 @@
+let formElement = document.querySelector('.poppup-container');
+let poppup = document.querySelector('.poppup');
+let nameInput = document.querySelector('.poppup-container__name');
+let jobInput = document.querySelector('.poppup-containter__profession');
+let poppup_close = document.querySelector('.poppup-container__button-close');
+let poppup_open = document.querySelector('.profile__button');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let profile__title = document.querySelector('.profile__title');
+let profile__profession = document.querySelector('.profile__profession');
+    profile__title.textContent = nameInput.value;
+    profile__profession.textContent = jobInput.value;
+    
+    poppup.classList.remove ('poppup__opened');
+    
+
+    
+}
+function poppupClose () {
+    
+    poppup.classList.remove('poppup__opened');
+    
+    
+
+}
+function poppupOpen () {
+    poppup.classList.add('poppup__opened');
+    
+}
+formElement.addEventListener('submit', formSubmitHandler); 
+poppup_close.addEventListener('click', poppupClose);
+poppup_open.addEventListener('click', poppupOpen);
