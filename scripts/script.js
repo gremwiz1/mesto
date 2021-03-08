ab67fcc890777e11e2d1df954e74a37240fd896e
Gremwiz <gremwiz@yandex.ru>
2021-03-08 12:53:47 +0300

    fix some mistakes and add change like active

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
-let formElement = document.getElementById('poppupProfile');
-let poppup = document.querySelector('.poppup');
-let nameInput = document.querySelector('input[name="firstname"]');
-let jobInput = document.querySelector('input[name="profession"]');
-let poppupIsClose = document.querySelectorAll('.poppup__button-close');
-let poppupIsOpen = document.querySelector('.profile-button');
-let profileTitle = document.querySelector('.profile__title');
-let profileProfession = document.querySelector('.profile__profession');
+const formElement = document.getElementById('poppupProfile');
+const poppupFormElement = document.getElementById('poppupFormElement');
+const poppup = document.querySelector('.poppup');
+const nameInput = document.querySelector('input[name="firstname"]');
+const jobInput = document.querySelector('input[name="profession"]');
+const poppupIsClose = document.querySelectorAll('.poppup__button-close');
+const poppupIsOpen = document.querySelector('.profile-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileProfession = document.querySelector('.profile__profession');
 const poppupAddElement = document.getElementById('poppupElement');
 const addElementButton = document.querySelector('.add-button');
 const poppupWithImage = document.getElementById('imagepopup');
 const poppupImage = document.querySelector('.poppup__image');
 const poppupText = document.querySelector('.poppup__text');
+const templateElement = document.querySelector('.template');
+const container = document.querySelector('.elements');
 const initialCards = [
     {
       name: 'Архыз',
@@ -37,9 +40,14 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ]; 
-const templateElement = document.querySelector('.template');
-const container = document.querySelector('.elements');
 renderElements();
+formElement.addEventListener('submit', formSubmitHandler);
+poppupFormElement.addEventListener('submit', addElement);
+poppupIsOpen.addEventListener('click', () => {poppupOpen(poppup)});
+addElementButton.addEventListener('click',() => {poppupOpen(poppupAddElement)});
+[].forEach.call(poppupIsClose, function(item) {
+    item.addEventListener('click', () => {poppupClose(item.parentNode.parentNode)});
+});
 function renderElements() {
 const result = initialCards.map(createTemplateElement);
 container.append(...result);
@@ -56,7 +64,6 @@ function createTemplateElement(item) {
     elementImage.addEventListener('click',() =>{imagePopup(elementImage, elementTitle)});
     elementLike.addEventListener('click',() => {changeLikeElement(elementLike)});
     elementButtonDelete.addEventListener('click', () => {deleteElement(elementButtonDelete)})
-
     return newItem;
 }
 function formSubmitHandler(evt) {
@@ -67,25 +74,17 @@ function formSubmitHandler(evt) {
 }
 function poppupClose(poppup) {
     poppup.classList.remove('poppup_opened');
-   
 }
 function poppupOpen(poppup) {
     poppup.classList.add('poppup_opened');
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileProfession.textContent;
 }
-formElement.addEventListener('submit', formSubmitHandler);
-poppupIsOpen.addEventListener('click', () => {poppupOpen(poppup)});
-addElementButton.addEventListener('click',() => {poppupOpen(poppupAddElement)});
-[].forEach.call(poppupIsClose, function(item) {
-    item.addEventListener('click', () => {poppupClose(item.parentNode.parentNode)});
-});
 function changeLikeElement(item) {
-item.classList.toggle('.element__like:active');
+item.classList.toggle('element__like-active');
 }
 function imagePopup(item, text) {
 poppupWithImage.classList.add('poppup_opened');    
-
 poppupImage.src = item.src;
 poppupImage.alt = item.alt;
 poppupText.textContent = text.textContent;
@@ -93,4 +92,14 @@ poppupText.textContent = text.textContent;
 function deleteElement(item) {
 const elementDelete = item.parentNode;
 elementDelete.remove();
+}
+function addElement(evt) {
+    evt.preventDefault();
+    const inputNameImage = document.querySelector('input[name="nameimage"]');
+    const inputSourceImage = document.querySelector('input[name="path"]');
+    const result = createTemplateElement({name: inputNameImage.value, link: inputSourceImage.value});
+    container.prepend(result);
+    inputNameImage.value ="";
+    inputSourceImage.value ="";
+    poppupClose(evt.target.parentNode.parentNode);
 }
