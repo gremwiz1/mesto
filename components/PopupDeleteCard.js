688afc5b7e0fa24959d61c304a986a14aee25137
Gremwiz <gremwiz@yandex.ru>
2021-05-07 16:09:08 +0300

    Fix class popupDeleteCard

--- components/PopupDeleteCard.js
+++ components/PopupDeleteCard.js
@@ -2,24 +2,28 @@ import { Popup } from './Popup.js';
 export class PopupDeleteCard extends Popup {
     constructor(popupElement) {
         super(popupElement);
-        
+
         this._form = popupElement.querySelector('[name="popup-container"]');
-        
+
     }
-    
+
     setEventListeners() {
         super.setEventListeners();
-        
-        this._form.addEventListener('submit', this.changeFunctionSubmit);
+
+        this._form.addEventListener('submit', () => {
+            evt.preventDefault();
+            this.changeFunctionSubmit();
+        })
+            
         
     }
     close() {
         super.close();
-        
+
     }
     open() {
         super.open();
-        
+
     }
     changeFunctionSubmit(handler) {
         this.changeFunctionSubmit = handler;
